8b366668d56d9204bb3136dc092e9109215c52da
personal <gunwant7272@gmail.com>
2022-12-20 21:31:27 +0530

    🐛 tailwind fixed

--- src/components/RuleCard.js
+++ src/components/RuleCard.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function RuleCard({title, description}) {
   return (
-    <div className='' >
-        <h1>{title}</h1>
-        <div>{description}</div>
+    <div className='border-2  m-2 w-48 ' >
+        <h1 className=' border-b-2 p-2 '>{title}</h1>
+        <div className='p-2' >{description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, error? </div>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
-import Head from 'next/head'
 import RuleCard from "../components/RuleCard"
 
 
 export default function Home() {
   return (
-    <div className='bg-slate-500 ' >
+    <div className='bg-slate-500  ' >
       hello world
      {/* rule section */}
-      <div>
+      <div className="flex" >
 
       <RuleCard title={"Rule1"} description="text des" />
       <RuleCard title={"Rule2"} description="text des" />
