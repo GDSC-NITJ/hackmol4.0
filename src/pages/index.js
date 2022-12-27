085ae5475e94d7fca8dbb923e3b09bd6bb1771de
aditygrg2 <uber6707@gmail.com>
2022-12-27 15:54:04 +0530

    Added Footer Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-
 import TrackAndPrices from "../components/TrackAndPrices"
 import FAQ from "../components/FAQ"
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
@@ -23,8 +23,12 @@ export default function Home() {
      {/* Tracks And Prices end */}
 
 
-    {/* {FAQ Section Starts} - Managed by Aditya - Junior*/}
+    {/*FAQ and footer starts*/}
+
     <FAQ></FAQ>
+    <Footer></Footer>
+
+    {/*FAQ and footer ends*/}
     </div>
 
   )
