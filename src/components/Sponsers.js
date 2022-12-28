77a30a39d12fcd804eb21a2617cee256599bd941
Aman <Palaman2626@gmail.com>
2022-12-28 18:21:30 +0530

    Sponsers Page

---
+++ src/components/Sponsers.js
@@ -0,0 +1,10 @@
+function Sponsers()
+{
+    return (
+    <div className="w-full h-screen flex justify-center items-center bg-black">
+        <div className="container flex justify-center w-3/4 h-1/2 text-white text-5xl shadow-5xl shadow-white font-extrabold border-white border-8 items-center">Sponsers-To be decided</div>
+    </div>
+    )
+}
+
+export default Sponsers;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,35 @@
-import TrackAndPrices from "../components/TrackAndPrices"
-import FAQ from "../components/FAQ"
+import TrackAndPrices from "../components/TrackAndPrices";
+import FAQ from "../components/FAQ";
 
-import Announcement from "../components/Announcement"
+import Announcement from "../components/Announcement";
 
+import Footer from "../components/Footer";
 
-import Footer from "../components/Footer"
-
-
-import RuleCard from "../components/RulesCard"
-
+import RuleCard from "../components/RulesCard";
+import Sponsers from "../components/Sponsers";
 
 export default function Home() {
   return (
-    <div className='' >
-
+    <div className="">
       {/* Announcement start */}
 
-      <Announcement/>
-
+      <Announcement />
 
       {/* Announcement ends */}
-     {/* rule section */}
-      <RuleCard/>
-     {/* rule section end */}
+      {/* rule section */}
+      <RuleCard />
+      {/* rule section end */}
 
-     {/* Tracks And Prices starts */}
-    <TrackAndPrices />
-     {/* Tracks And Prices end */}
+      {/* Tracks And Prices starts */}
+      <TrackAndPrices />
+      {/* Tracks And Prices end */}
 
+      {/*FAQ and footer starts*/}
+      <Sponsers></Sponsers>
+      <FAQ></FAQ>
+      <Footer></Footer>
 
-    {/*FAQ and footer starts*/}
-
-    <FAQ></FAQ>
-    <Footer></Footer>
-
-    {/*FAQ and footer ends*/}
+      {/*FAQ and footer ends*/}
     </div>
-
-  )
+  );
 }
