676de68966fb8f9bd8fd08bba2844c0e047c5a13
Gunwant Wankhede <work@gunwant.wankhede>
2022-12-30 15:31:05 +0530

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -58,9 +58,6 @@ const Banner = () => {
                 <br></br>
                 <br></br>
                 {/* <h1><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Connect", "Build", "Innovate" ]'><span className="wrap">{text}</span></span></h1> */}
-
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-=======
                   <p>HackMOL is the flagship annual 30-hour hackathon being organised by Google Developer Student (GDSC) of NIT Jalandhar wherein young coders & developers from all over the country join together to build projects & solutions to the alarming problems of the region & the world. </p>
 
                   <div className="banner__buttons__one">
