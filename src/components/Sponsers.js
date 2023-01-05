67c6a38029596b4a3be50ba3987f59918f57d127
Ajay <aky11052003@gmail.com>
2023-01-05 19:27:51 +0530

    first commit sponsers page

--- src/components/Sponsers.js
+++ src/components/Sponsers.js
@@ -1,10 +1,69 @@
-function Sponsers()
-{
-    return (
-    <div className="w-full py-16 md:py-20 flex justify-center items-center bg-purple ">
-        <h1 className="container flex justify-center w-3/4 h-80 text-white  text-3xl md:text-4xl font-extrabold shadow-bordered-black text-center border-white border-2 items-center">Sponsers-To be decided</h1>
+import react from "react";
+
+function Sponsers() {
+  return (
+    <div className="sponsers">
+
+      <div className="sponsers-heading">SPONSERS</div>
+
+      <div className="diamond__sponsers">
+        <div className="diamond__sponsers-heading">
+          <span>Diamond Sponsers</span>
+        </div>
+        <div className="diamond__sponsers-sponsers">
+          <div className="diamond__sponsers-sponsers-box"></div>
+          <div className="diamond__sponsers-sponsers-box"></div>
+        </div>
+      </div>
+   
+      <div className="gold__sponsers">
+        <div className="gold__sponsers-heading">
+          <span>Gold Sponsers</span>
+        </div>
+        <div className="gold__sponsers-sponsers">
+          <div className="gold__sponsers-sponsers-box"></div>
+          <div className="gold__sponsers-sponsers-box"></div>
+          <div className="gold__sponsers-sponsers-box"></div>
+          <div className="gold__sponsers-sponsers-box"></div>
+          <div className="gold__sponsers-sponsers-box"></div>
+          <div className="gold__sponsers-sponsers-box"></div>
+        </div>
+      </div>
+
+      <div className="silver__sponsers">
+        <div className="silver__sponsers-heading">
+          <span>Siver Sponsers</span>
+        </div>
+        <div className="silver__sponsers-sponsers">
+          <div className="silver__sponsers-sponsers-box"></div>
+          <div className="silver__sponsers-sponsers-box"></div>
+          <div className="silver__sponsers-sponsers-box"></div>
+          <div className="silver__sponsers-sponsers-box"></div>
+          <div className="silver__sponsers-sponsers-box"></div>
+          <div className="silver__sponsers-sponsers-box"></div>
+        </div>
+      </div>
+
+      <div className="bronze__sponsers">
+        <div className="bronze__sponsers-heading">
+          <span>Bronze Sponsers</span>
+        </div>
+        <div className="bronze__sponsers-sponsers">
+          <div className="bronze__sponsers-sponsers-box"></div>
+          <div className="bronze__sponsers-sponsers-box"></div>
+          <div className="bronze__sponsers-sponsers-box"></div>
+          <div className="bronze__sponsers-sponsers-box"></div>
+          <div className="bronze__sponsers-sponsers-box"></div>
+          <div className="bronze__sponsers-sponsers-box"></div>
+          <div className="bronze__sponsers-sponsers-box"></div>
+          <div className="bronze__sponsers-sponsers-box"></div>
+          <div className="bronze__sponsers-sponsers-box"></div>
+          <div className="bronze__sponsers-sponsers-box"></div>
+        </div>
+      </div>
+
     </div>
-    )
+  );
 }
 
 export default Sponsers;
