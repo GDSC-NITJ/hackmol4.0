7e64f8c2e40af6bb4ba2de52864515069b77eedc
Ajay <aky11052003@gmail.com>
2023-01-05 21:53:49 +0530

    added sponsers

--- src/components/Sponsers.js
+++ src/components/Sponsers.js
@@ -2,6 +2,12 @@ import react from "react";
 import devfolio from '../../public/images/sponsors/devfolio.png'
 import gfg from '../../public/images/sponsors/gfg.png'
 import celo from '../../public/images/sponsors/celo.png'
+import filecoin from '../../public/images/sponsors/filecoin.png'
+import polygon from '../../public/images/sponsors/polygon.png'
+import tezos from '../../public/images/sponsors/tezos.png'
+import xyz from '../../public/images/sponsors/xyz.png'
+import liib from '../../public/images/sponsors/liib.png'
+import taskade from '../../public/images/sponsors/taskade.png'
 
 function Sponsers() {
   return (
@@ -14,8 +20,8 @@ function Sponsers() {
           <span>Diamond Sponsers</span>
         </div>
         <div className="diamond__sponsers-sponsers">
-          <div className="diamond__sponsers-sponsers-box"><img src={devfolio.src}/></div>
-          <div className="diamond__sponsers-sponsers-box"><img src={gfg.src}/></div>
+          <div className="diamond__sponsers-sponsers-box"><img src={devfolio.src} /></div>
+          <div className="diamond__sponsers-sponsers-box"><img src={polygon.src}/></div>
         </div>
       </div>
    
@@ -24,12 +30,12 @@ function Sponsers() {
           <span>Gold Sponsers</span>
         </div>
         <div className="gold__sponsers-sponsers">
-          <div className="gold__sponsers-sponsers-box"><img src={devfolio.src}/></div>
+          <div className="gold__sponsers-sponsers-box"><img src={celo.src}/></div>
+          <div className="gold__sponsers-sponsers-box"><img src={filecoin.src}/></div>
           <div className="gold__sponsers-sponsers-box"><img src={gfg.src}/></div>
-          <div className="gold__sponsers-sponsers-box"><img src={devfolio.src}/></div>
-          <div className="gold__sponsers-sponsers-box"><img src={gfg.src}/></div>
-          <div className="gold__sponsers-sponsers-box"><img src={devfolio.src}/></div>
-          <div className="gold__sponsers-sponsers-box"><img src={gfg.src}/></div>
+          <div className="gold__sponsers-sponsers-box"><img src={tezos.src}/></div>
+          <div className="gold__sponsers-sponsers-box"><img src={xyz.src}/></div>
+          <div className="gold__sponsers-sponsers-box"><img src={liib.src}/></div>
         </div>
       </div>
 
@@ -38,12 +44,12 @@ function Sponsers() {
           <span>Siver Sponsers</span>
         </div>
         <div className="silver__sponsers-sponsers">
-          <div className="silver__sponsers-sponsers-box"><img src={devfolio.src}/></div>
+          <div className="silver__sponsers-sponsers-box"><img src={taskade.src}/></div>
+          <div className="silver__sponsers-sponsers-box"><img src={filecoin.src}/></div>
           <div className="silver__sponsers-sponsers-box"><img src={gfg.src}/></div>
-          <div className="silver__sponsers-sponsers-box"><img src={celo.src}/></div>
-          <div className="silver__sponsers-sponsers-box"><img src={celo.src}/></div>
-          <div className="silver__sponsers-sponsers-box"><img src={devfolio.src}/></div>
-          <div className="silver__sponsers-sponsers-box"><img src={celo.src}/></div>
+          <div className="silver__sponsers-sponsers-box"><img src={tezos.src}/></div>
+          <div className="silver__sponsers-sponsers-box"><img src={xyz.src}/></div>
+          <div className="silver__sponsers-sponsers-box"><img src={liib.src}/></div>
         </div>
       </div>
 
