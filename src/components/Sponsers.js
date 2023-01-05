9e2785f85ca71b57fc9593cc76707e5109437e53
NIKU-SINGH <soapmactavishmw4@gmail.com>
2023-01-06 01:40:34 +0530

    improved sponsor section

--- src/components/Sponsers.js
+++ src/components/Sponsers.js
@@ -2,21 +2,21 @@ import devfolio from "../../public/images/sponsors/devfolio.png"
 
 function Sponsers() {
     return (
-        <div className="w-full py-16 md:py-20 flex justify-center items-center bg-gray-100 flex-col ">
+        <div className="w-full py-16 md:py-20 flex justify-center items-center bg-gray-100 flex-col px-10">
             {/* <h1 className="container flex justify-center w-3/4 h-80 text-white text-3xl md:text-4xl font-extrabold shadow-bordered-black text-center border-white border-2 items-center">
             Sponsers
         </h1>    */}
             <h1 className="text-4xl uppercase  mb-6 font-bold font-Space-Grotesk">Sponsors</h1>
 
             <div className="flex items-center justify-center flex-col p-2">
-                <div className="mt-12 font-Red-Hat-Display font-semibold text-base sm:w-[294px] sm:h-11 bg-[#FFC900] text-center px-10 py-2 shadow-3xl">
+                <div className="mt-12 font-Red-Hat-Display font-semibold text-base w-[294px] h-11 bg-[#FFC900] text-center px-10 py-2 shadow-3xl">
                     Diamond Sponsors
                 </div>
                 <div className=" flex items-center flex-wrap justify-center">
-                    <div className="w-96 h-24  sm:w-[366px] md:h-[104px] mt-10 mx-4 flex items-center justify-center rounded-xl border-2 border-black shadow-3xl p-2 text-black bg-white ">
+                    <div className="w-64 h-24  sm:w-[366px] md:h-[104px] mt-10 mx-4 flex items-center justify-center rounded-xl border-2 border-black shadow-3xl p-2 text-black bg-white ">
                         <img src={devfolio.src} alt="Sponsor Image" className="p-2" />
                     </div>
-                    <div className="w-96 h-24 sm:w-[366px] md:h-[104px]  mt-10 mx-4 flex items-center justify-center rounded-xl border-2 border-black shadow-3xl p-2 text-black bg-white ">
+                    <div className="w-64 h-24 sm:w-[366px] md:h-[104px]  mt-10 mx-4 flex items-center justify-center rounded-xl border-2 border-black shadow-3xl p-2 text-black bg-white ">
                         <img src={devfolio.src} alt="Sponsor Image" className="p-2" />
                     </div>
                 </div>
