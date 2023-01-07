0de186c896ab077dae356226147919722fecc41b
Gunwant Wankhede <work@gunwant.wankhede>
2023-01-07 23:14:22 +0530

    upfdarte

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -8,16 +8,18 @@ const Announcement = () => {
                 <div>
                     <h1 className="justify-center h-1/2 text-black  text-3xl md:text-4xl font-extrabold items-center pb-4 text-center ">Announcement</h1>
                 </div>
-                <div className="font-semibold text-center border-2 border-black p-10 shadow-3xl bg-white rounded-xl w-11/12 sm:w-9/12 text-lg ">
+                <div className="font-semibold sm:text-center border-2 border-black p-10 shadow-3xl bg-white rounded-xl w-11/12 sm:w-9/12 text-lg ">
                 To be eligible for fresher&apos;strack, all the members of team should be from first year of Degree/ All school students are also eligible for hackathon
 
 Opting for fresher&apos;strack is a choice, instead of compulsion so if your team wish to participate in main track you can, we&apos;ll roll out forms for this choice during hackathon
 
-Folks participating in fresher&apos;strack can start their project before hackathon period
-
-You can use any open source library/module/framework to build your hack
-
-You can build a hack out of any Idea, we will not provide any Idea, There are no restrictions on Ideas
+Folks participating in fresher&apos;strack can start their project before hackathon period. 
+<span className="hidden sm:inline" >
+    
+    You can use any open source library/module/framework to build your hack
+    
+    You can build a hack out of any Idea, we will not provide any Idea, There are no restrictions on Ideas
+</span>
 
 
                 </div>
