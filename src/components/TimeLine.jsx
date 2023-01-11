26677229640c9872755fcfdf151d8a9f182bf7e2
Gunwant Wankhede <work@gunwant.wankhede>
2023-01-11 21:12:46 +0530

    build fix

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -32,9 +32,10 @@ function TimeLine() {
           TimeLine
         </h1>
         <div class='border-l-2 border-yellow mt-10'>
-          {obj.map((day) => {
+          {obj.map((day, i) => {
             return (
               <div
+                key={i}
                 class={`transform transition shadow-3xl cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-${day.bgColor} text-white rounded-xl mb-10 flex-col md:flex-row space-y-4 md:space-y-0`}>
                 <div
                   class={`w-5 h-5 bg-${day.bgColor} absolute -left-10 transform -translate-x-2/4 top-12 rounded-full z-10 mt-2 md:mt-0`}></div>
