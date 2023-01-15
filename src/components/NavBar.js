d4007f6a7c80efd35ff956c84b6c7283baf2bbf5
Gunwant Wankhede <work@gunwant.wankhede>
2023-01-15 17:59:24 +0530

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -128,14 +128,7 @@ const NavBar = () => {
                       PRIZES
                     </Link>
                   </li>
-                  <li className="text-white text-extrabold font-bold  md:text-xl hover:text-indigo-200">
-                    <Link
-                      href={'/MOL-web'}
-                      scroll={false}
-                    >
-                     MOL
-                    </Link>
-                  </li>
+                 
                   <li className="text-white text-extrabold font-bold  md:text-xl hover:text-indigo-200">
                     <Link
                       href={'#contact'}
