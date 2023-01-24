92e5631b898f5290cd7521e42ea1be19c71ef29a
aditygrg2 <uber6707@gmail.com>
2023-01-24 21:16:18 +0530

    Updated

--- src/components/PastYearStats.js
+++ src/components/PastYearStats.js
@@ -3,18 +3,18 @@ import img from '../../public/past-year.png'
 
 const PastYearStats =  function PastYearStats() {
   return (
-    <div className='flex flex-row w-full border-2 border-black flex-wrap'>
-        <div className='bg-[#7B61FF] border-r-2 border-black h-[741px] flex-grow'>
-            <img src={img.src} className='m-auto relative top-1/2 -translate-y-1/2 p-8'></img>
+    <div className='flex flex-col md:flex-row w-full border-2 border-black'>
+        <div className='bg-[#7B61FF] border-r-2 border-black h-[400px] md:h-[741px] flex-grow'>
+            <img src={img.src} className='m-auto relative top-1/2 -translate-y-1/2 p-4 md:p-8'></img>
         </div>
-        <div className='h-[741px] flex-grow p-4'>
-            <h1 className='font-Space-Grotesk font-bold relative -translate-y-[15%] top-[15%] text-5xl text-center tracking-wider'>How it went last year ?</h1>
-            <ul className='top-[40%] relative -translate-y-[40%] sm:left-[40px] w-full'>
-                <li className='past-stats-list font-normal text-3xl leading-[2] flex gap-6'> 1200+ Registrations Test</li>
-                <li className='past-stats-list font-normal text-3xl leading-[2] flex gap-6'> 700+ participation for Freshers track</li>
-                <li className='past-stats-list font-normal text-3xl leading-[2] flex gap-6'> 500+ participation in the Main track</li>
-                <li className='past-stats-list font-normal text-3xl leading-[2] flex gap-6'> 30+ sponsors participated</li>
-                <li className='past-stats-list font-normal text-3xl leading-[2] flex gap-6'> 75+ project submissions</li>
+        <div className='h-[700px] md:h-[741px] flex-grow p-4'>
+            <h1 className='font-Space-Grotesk font-bold relative -translate-y-[10%] top-[10%] md:-translate-y-[15%] md:top-[15%] md:text-5xl text-3xl text-center md:tracking-wider p-4'>How it went last year ?</h1>
+            <ul className='top-[40%] relative -translate-y-[40%]'>
+                <li className='past-stats-list pl-[8%] font-normal pb-2  md:pb-4 md:text-3xl text-xl leading-[2] flex gap-3 md:gap-6'> 1200+ Registrations Test</li>
+                <li className='past-stats-list pl-[8%] font-normal pb-2 md:pb-4 md:text-3xl text-xl leading-[2] flex gap-3 md:gap-6'> 700+ participation for Freshers track</li>
+                <li className='past-stats-list pl-[8%] font-normal pb-2 md:pb-4 md:text-3xl text-xl leading-[2] flex gap-3 md:gap-6'> 500+ participation in the Main track</li>
+                <li className='past-stats-list pl-[8%] font-normal pb-2 md:pb-4 md:text-3xl text-xl leading-[2] flex gap-3 md:gap-6'> 30+ sponsors participated</li>
+                <li className='past-stats-list pl-[8%] font-normal pb-2 md:pb-4 md:text-3xl text-xl leading-[2] flex gap-3 md:gap-6'> 75+ project submissions</li>
             </ul>
         </div>
     </div>
