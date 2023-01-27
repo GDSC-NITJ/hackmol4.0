6a6a93c14bc624b4522d6df3b906b75b6024167f
Nishchay <nishchayveer19@gmail.com>
2023-01-27 12:34:22 +0530

    padding changed

--- src/components/Footer/FooterLinks.js
+++ src/components/Footer/FooterLinks.js
@@ -5,11 +5,11 @@ let FooterLinks = function(props){
     return (
         <div className='md:flex hidden bg-black text-white h-[72px] justify-center items-center w-full font-Jakarta font-light'>
             {/* <div className='p-32'> */}
-                <Link className="hover:underline mx-8" href="#">Contact Us</Link>
-                <Link className="hover:underline mx-8" href="#">University</Link>
-                <Link className="hover:underline mx-8" href="#">Website</Link>
-                <Link className="hover:underline mx-8" href="#">Terms of Service</Link>
-                <Link className="hover:underline mx-8" href="#">Privacy Policy</Link>
+                <Link className="hover:underline mx-4" href="#">Contact Us</Link>
+                <Link className="hover:underline mx-4" href="#">University</Link>
+                <Link className="hover:underline mx-4" href="#">Website</Link>
+                <Link className="hover:underline mx-4" href="#">Terms of Service</Link>
+                <Link className="hover:underline mx-4" href="#">Privacy Policy</Link>
             {/* </div> */}
         </div>
     )
